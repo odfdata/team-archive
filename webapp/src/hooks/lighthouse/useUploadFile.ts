7e778d2356d241faa865924caf2d37c2faff81f6
Federico Castelli <fc@oracleofde.fi>
2023-01-26 12:29:09 +0100

    Create useUploadFile() hook logic

--- webapp/src/hooks/lighthouse/useUploadFile.ts
+++ webapp/src/hooks/lighthouse/useUploadFile.ts
@@ -1,9 +1,17 @@
 import {useBaseAsyncHook, useBaseAsyncHookState} from "../utils/useBaseAsyncHook";
+import {useEffect} from "react";
+import lighthouse from '@lighthouse-web3/sdk';
 
+/**
+ * @param {string} CID - The CID file uploaded using Lighthouse
+ */
 export interface UploadedFileDetails {
-
+  CID: string;
 }
 
+/**
+ * @param {File} file - The file to be uploaded using Lighthouse
+ */
 export interface UploadFileParams {
   file: File
 }
@@ -14,5 +22,24 @@ export interface UploadFileParams {
 export const useUploadFile = (params: UploadFileParams): useBaseAsyncHookState<UploadedFileDetails> => {
   const { completed, error, loading, progress, result,
     startAsyncAction, endAsyncActionSuccess, updateAsyncActionProgress } = useBaseAsyncHook<UploadedFileDetails>();
+
+  useEffect(() => {
+    startAsyncAction();
+    const progressCallback = (progressData) => {
+      const percentageDone = 100 - parseInt((progressData.total / progressData.uploaded).toFixed(2));
+      uploadPercentage(percentageDone);
+    };
+
+    const output = lighthouse.upload(
+      params.file.webkitRelativePath, process.env.LIGHTHOUSE_API_KEY, progressCallback)
+      .then(data => { endAsyncActionSuccess({
+        CID: data.data.Hash,
+      })});
+  }, [params.file]);
+
+  const uploadPercentage = (percentage: number): void => {
+    updateAsyncActionProgress(percentage);
+  };
+
   return { completed, error, loading, progress, result };
 };
