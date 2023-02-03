5fe38dafa630ff1e9b25a6c02473f8be9caa3dc8
Antonio <antonio.giovanni.seveso@gmail.com>
2023-02-03 11:31:51 +0100

    - drafted all smart contracts

--- webapp/src/ui/pages/Home/Home.tsx
+++ webapp/src/ui/pages/Home/Home.tsx
@@ -20,8 +20,9 @@ const Home: React.FC<IHome> = (props) => {
 
   return (
     <Box width={"100%"} minHeight={"100vh"}
-         display={"flex"} flexDirection={"column"} alignItems={"center"} pt={20}>
-      <h1>Team Archive</h1>
+         display={"flex"} flexDirection={"column"} alignItems={"center"} pt={15}>
+      <img src={"/team-archive-logo.png"} style={{width: 120}}/>
+      <h1 style={{marginBottom: 0}}>Team Archive</h1>
       <Typography variant="subtitle1">
         Decentralized web3 storage platform
       </Typography>
